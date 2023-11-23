c8bc580ab3320ff13fac383c12e588ee4b28f36a
Andrei Marshalov <andrei.marshalov@emlid.com>
2023-11-23 10:39:44 +0100

    Fixed

--- worker/index.js
+++ worker/index.js
@@ -1,4 +1,4 @@
-const keys = require('./keys.js');
+const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
@@ -10,7 +10,7 @@ const sub = redisClient.duplicate();
 
 function fib(index) {
   if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 1);
+  return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
